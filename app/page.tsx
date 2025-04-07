9020c1029c65fe65419f48a934efdec7081e4027
markbang <i@bangwu.top>
2025-04-07 23:05:49 +0800

    feat: enhance Tauri application with additional plugins and update configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
@@ -17,10 +18,18 @@ export default function Home() {
       <main className='flex flex-col items-center justify-between p-24'>
         <h2 className='text-2xl font-bold'>Get started</h2>
         <div className='flex flex-col items-center justify-center mt-4 space-y-2 underline'>
-          <a href='./dashboard'>Go to Dash</a>
-          <a href='./login'>Login</a>
-          <a href='./register'>Register</a>
-          <a href='./forgot-password'>Forgot Password</a>
+          <Button variant='default' className='w-48'>
+            <a href='/docs/getting-started'>Getting Started</a>
+          </Button>
+          <Button variant='secondary' className='w-48'>
+            <a href='/docs/api'>API Reference</a>
+          </Button>
+          <Button variant='link' className='w-48'>
+            <a href='/login'>登录</a>
+          </Button>
+          <Button variant='outline' className='w-48'>
+            <a href='/dashboard'>Dashboard</a>
+          </Button>
         </div>
       </main>
       <footer className='flex items-center justify-center h-24 border-t'>
