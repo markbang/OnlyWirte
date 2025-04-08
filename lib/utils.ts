f4bb59dfa51dfbf7f840debec388670c8557487f
markbang <i@bangwu.top>
2025-04-08 14:14:04 +0800

    feat: change homepage style and add version

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { version } from '../package.json';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getversion() {
+  return version;
 }
