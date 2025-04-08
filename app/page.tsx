fe8e78130e0c83ed4a4047289ff35481a92302b4
Bangwu <119404476+markbang@users.noreply.github.com>
2025-04-08 10:10:24 +0800

    feat: add updater support (#1)
    * feat: enhance Tauri application with additional plugins and update configuration
    
    * feat: add updater window and button
    
    * fix: test-build PRIVATE_KEY error
    
    * fix: error running bundle_dmg.sh use[--bundles app](https://github.com/tauri-apps/tauri-action/issues/801)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import { useEffect } from 'react';
+import { checkForAppUpdates } from '@/lib/updater';
 
 export default function Home() {
+  // 使用 useEffect 在组件挂载时检查更新
+  useEffect(() => {
+    checkForAppUpdates(false);
+  }, []);
+
+  // 处理点击检查更新按钮
+  const handleCheckUpdate = () => {
+    checkForAppUpdates(false);
+  };
+
   return (
     <>
       <header className='flex flex-col items-center justify-center bg-gray-100'>
@@ -17,10 +32,22 @@ export default function Home() {
       <main className='flex flex-col items-center justify-between p-24'>
         <h2 className='text-2xl font-bold'>Get started</h2>
         <div className='flex flex-col items-center justify-center mt-4 space-y-2 underline'>
-          <a href='./dashboard'>Go to Dash</a>
-          <a href='./login'>Login</a>
-          <a href='./register'>Register</a>
-          <a href='./forgot-password'>Forgot Password</a>
+          <Button variant='default' className='w-48'>
+            <a href='/docs/getting-started'>Getting Started</a>
+          </Button>
+          <Button
+            variant='secondary'
+            className='w-48'
+            onClick={handleCheckUpdate}
+          >
+            检查更新
+          </Button>
+          <Button variant='link' className='w-48'>
+            <a href='/login'>登录</a>
+          </Button>
+          <Button variant='outline' className='w-48'>
+            <a href='/dashboard'>Dashboard</a>
+          </Button>
         </div>
       </main>
       <footer className='flex items-center justify-center h-24 border-t'>
